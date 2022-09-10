4cfdda3d285af49d78251ad4129d789bc741cdca
Hans Elliott <hanselliott61@gmail.com>
2022-09-10 13:33:56 -0700

    finish surfcam

--- harborvista_webcam/produce_gif.py
+++ harborvista_webcam/produce_gif.py
@@ -12,22 +12,24 @@ import sys
 def scrape_frames(n_frames=30):
     """
     Get latest frame from the web cam URL. Repeat n_frames times and save in list.
+    For 30 frames, time ~= 1m 25.5s
     """
     frames = []
-    for _ in range(n_frames):
+    for i in range(n_frames):
         response = requests.get("https://apps.lanecounty.org/LCWebCams/HarborVista")
         frames += [np.array( Image.open(BytesIO(response.content)).convert('RGB') )]
+        sys.stdout.write("\r frame {0} ".format(str(i+1)))
+        #sys.stdout.flush()
 
     return frames
-    ## for 30 frames, time = 1m 25.5s 
 
 
 if __name__ == '__main__':
-    # if len(sys.argv) >:
-    n_frames = int(sys.argv[1])
-    # else:
-    #     n_frames = 30
-
+    if len(sys.argv) > 1:
+        n_frames = int(sys.argv[1])
+    else:
+        n_frames = 30
+    
     imageio.mimsave(f'C:/Users/hanse/Documents/py-bits/harborvista_webcam/gifs/{strftime("%m-%d_%H.%M", localtime())}_frames.gif', 
                     scrape_frames(n_frames),
                     fps=0.75)
